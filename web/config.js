79f2fcc75184ee4a52492942887a69106e01949b
Jingzhong Lin <134421090+RipeMangoBox@users.noreply.github.com>
2026-09-20 12:04:17 +0800

    Restore public questionnaire tunnel after network interruption

--- web/config.js
+++ web/config.js
@@ -1,2 +1,2 @@
 // Empty uses the server serving this page. GitHub Pages may set the API origin.
-window.STUDY_API = 'https://1f615aa6c39a3de9c0.gradio.live';
+window.STUDY_API = 'https://9ca445fd913d031584.gradio.live';
