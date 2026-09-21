1903dff1e3ee2e110f205bdf59dbeaf859d22ead
Jingzhong Lin <134421090+RipeMangoBox@users.noreply.github.com>
2026-09-21 14:43:06 +0800

    Switch questionnaire API and videos to persistent public hosting

--- web/config.js
+++ web/config.js
@@ -1,2 +1,2 @@
 // Empty uses the server serving this page. GitHub Pages may set the API origin.
-window.STUDY_API = 'https://9ca445fd913d031584.gradio.live';
+window.STUDY_API = 'https://aesop-user-study.ripemangobox.chatgpt.site';
